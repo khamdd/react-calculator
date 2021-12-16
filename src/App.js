0fb6f8e37b232d1934f7ccc42ea1b6553e08e69c
Kham <dangduongkham1714@gmail.com>
2021-12-16 15:31:31 +0700

    Test components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/calculator/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/calculator/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+function Header() {
+    return (
+        <div>
+            <h2>Test component Header</h2>
+            <Button />
+        </div>
+    )
+}
+
+export default Header
